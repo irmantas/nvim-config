975fb94123ba84e46c95c9a4bd8856e4923b2549
Irmantas Šiupšinskas <i.siupsinskas@gmail.com>
2022-06-20 01:56:20 +0300

    feat(icons): global icons plugin

--- lua/ark/plugins.lua
+++ lua/ark/plugins.lua
@@ -1,3 +1,4 @@
+-- Install packer plugin on plugins.lua file save
 vim.cmd([[
   augroup packer_user_config
     autocmd!
@@ -5,11 +6,13 @@ vim.cmd([[
   augroup end
 ]])
 
+-- safe call
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
     return
 end
 
+-- configure floating window for plugin install/update statuks
 packer.init {
     display = {
         open_fn = function()
@@ -18,22 +21,31 @@ packer.init {
     },
 }
 
+-- all plugins goes here
 return require('packer').startup(function(use)
+    --> Plugin manager
     -- self manage Packer
     use 'wbthomason/packer.nvim'
+    --< Plugin manager
 
-    -- Global requirements
+    --> Global requirements
+    -- base (used by many plugins) floating windows and other decor
     use 'nvim-lua/plenary.nvim'
+    -- icons from patched Nerd fonts (used by many plugins)
+    use 'kyazdani42/nvim-web-devicons'
+    --< Global requirements
 
-    -- TreeSitter
+    --> Editor behaviour
+    -- TreeSitter syntax highlighting
     use {
         'nvim-treesitter/nvim-treesitter',
 	    run = ':TSUpdate'
     }
-
     -- Editorconfig file support
     use 'gpanders/editorconfig.nvim'
+    --< Editor behaviour
 
-    -- Color Schemes
+    --> Color Schemes
     use 'luisiacc/gruvbox-baby'
+    --< Color Schemes
 end)
