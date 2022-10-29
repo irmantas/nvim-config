65620bd45eedb79dd3b684dc6adb511174fa77c0
Irmantas Šiupšinskas <i.siupsinskas@gmail.com>
2022-10-30 00:53:10 +0300

    chore: update plugin configuration

--- lua/plugins/lsp/null-ls.lua
+++ lua/plugins/lsp/null-ls.lua
@@ -6,13 +6,13 @@ end
 
 null_ls.setup {
     sources = {
-        null_ls.builtins.code_actions.xo,
+        -- null_ls.builtins.code_actions.xo,
         null_ls.builtins.diagnostics.eslint,
         --         null_ls.builtins.diagnostics.codespell,
         null_ls.builtins.diagnostics.jsonlint,
         null_ls.builtins.diagnostics.php,
         null_ls.builtins.diagnostics.shellcheck,
-        null_ls.builtins.diagnostics.xo,
+        -- null_ls.builtins.diagnostics.xo,
         null_ls.builtins.diagnostics.zsh,
         null_ls.builtins.formatting.black,
         null_ls.builtins.formatting.deno_fmt,
